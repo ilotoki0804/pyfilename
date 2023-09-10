502cc4247b805be9bc28dc77eb7350194897f430
ilotoki0804 <ilotoki0804@gmail.com>
2023-09-10 21:42:23 +0900

    코드 변경

--- pyfilename/__init__.py
+++ pyfilename/__init__.py
@@ -1,7 +1,21 @@
 if __name__ == "__main__":
-    import pyfilename
+    from pyfilename import (
+        TRANSLATE_TABLE_FULLWIDTH, TRANSLATE_TABLE_REPLACEMENT, NOT_ALLOWED_NAMES,
+        DOT_REMOVE, DOT_REPLACE, DOT_NO_CORRECTION, FOLLOWING_DOT_REPLACEMENT,
+        MODE_FULLWIDTH, MODE_USE_REPLACEMENT_CHAR, MODE_REMOVE,
+        CHAR_SPACE, CHAR_DOUBLE_QUOTATION_MARK, CHAR_WHITE_QUESTION_MARK, CHAR_RED_QUESTION_MARK,
+        EmptyStringError,
+        is_vaild_file_name, safe_name_to_original_name, translate_to_safe_path_name, translate_to_safe_name,
+    )
 else:
-    from . import pyfilename
+    from .pyfilename import (
+        TRANSLATE_TABLE_FULLWIDTH, TRANSLATE_TABLE_REPLACEMENT, NOT_ALLOWED_NAMES,
+        DOT_REMOVE, DOT_REPLACE, DOT_NO_CORRECTION, FOLLOWING_DOT_REPLACEMENT,
+        MODE_FULLWIDTH, MODE_USE_REPLACEMENT_CHAR, MODE_REMOVE,
+        CHAR_SPACE, CHAR_DOUBLE_QUOTATION_MARK, CHAR_WHITE_QUESTION_MARK, CHAR_RED_QUESTION_MARK,
+        EmptyStringError,
+        is_vaild_file_name, safe_name_to_original_name, translate_to_safe_path_name, translate_to_safe_name,
+    )
 
 if __name__ == '__main__':
     print('Testing codes.')
--- pyfilename/pyfilename.py
+++ pyfilename/pyfilename.py
@@ -1,127 +1,207 @@
-# TODO: replacement charactor
+# NTFS 예약어는 루트 디렉토리 (C:나 D: 등)에서 문제를 일으킬 수 있습니다.
+# 하지만 사용성이 매우 적기에 포함하지는 않았습니다.
+# 관련 링크: https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-fscc/b04c3bd0-79dc-4e58-b8ed-74f19fc2ea0a
+
+# 리눅스의 경우에는 NULL과 slash를 제외하면 모든 것을 파일 이름으로 사용할 수 있습니다.
+# 하지만 몇몇 기기들은 자체적으로 그 외의 이름에도 제한을 걸기도 합니다(삼성 갤럭시 등).
+
 import re
 import html
 from typing import Literal
 import logging
 from pathlib import Path
+from typing import overload, Final
 
-def safe_name(
-    name: str | Path,
-    name_property: Literal['file', 'path', 'posix_path', 'windows_path'] = 'file', # path: \와 / 모두 제외한다.
-    mode: Literal['fullwidth', 'space', 'remove'] = 'fullwidth',
+__all__ = (
+    "TRANSLATE_TABLE_FULLWIDTH", "TRANSLATE_TABLE_REPLACEMENT", "NOT_ALLOWED_NAMES",
+    "DOT_REMOVE", "DOT_REPLACE", "DOT_NO_CORRECTION", "FOLLOWING_DOT_REPLACEMENT",
+    "MODE_FULLWIDTH", "MODE_USE_REPLACEMENT_CHAR", "MODE_REMOVE",
+    "CHAR_SPACE", "CHAR_DOUBLE_QUOTATION_MARK", "CHAR_WHITE_QUESTION_MARK", "CHAR_RED_QUESTION_MARK",
+    "EmptyStringError",
+    "is_vaild_file_name", "safe_name_to_original_name", "translate_to_safe_path_name", "translate_to_safe_name",
+)
+
+TRANSLATE_TABLE_FULLWIDTH = str.maketrans('\\/:*?"<>|', '⧵／：＊？＂＜＞∣') | {i: 0 for i in range(32)}
+TRANSLATE_TABLE_REPLACEMENT = str.maketrans('\\/:*?"<>|', '\x00' * 9) | {i: 0 for i in range(32)}
+NOT_ALLOWED_NAMES = {
+    # 출처: https://learn.microsoft.com/en-us/windows/win32/fileio/naming-a-file
+
+    "CON", "PRN", "AUX", "NUL",
+
+    # "COM0",  # : 문서에는 사용할 수 없다고 나와있는데 실제로는 작동하기에 주석 처리함.
+    "COM1", "COM¹", "COM2", "COM²", "COM3", "COM³", "COM4", "COM5", "COM6", "COM7", "COM8", "COM9",
+
+    # "LPT0",  # : 문서에는 사용할 수 없다고 나와있는데 실제로는 작동하기에 주석 처리함.
+    "LPT1", "LPT¹", "LPT2", "LPT²", "LPT3", "LPT³", "LPT4", "LPT5", "LPT6", "LPT7", "LPT8", "LPT9",
+}
+
+DOT_REMOVE = 'dot_remove'
+DOT_REPLACE = 'dot_replace'
+DOT_NO_CORRECTION = 'dot_no_correction'
+FOLLOWING_DOT_REPLACEMENT = {DOT_REPLACE: '．', DOT_NO_CORRECTION: '.'}
+CorrectFollowingDot = Literal['dot_remove', 'dot_replace', 'dot_no_correction']
+
+MODE_FULLWIDTH = 'mode_fullwidth'
+MODE_USE_REPLACEMENT_CHAR = 'mode_use_replacement_char'
+MODE_REMOVE = 'mode_remove'
+TextModes = Literal['mode_fullwidth', 'mode_use_replacement_char', 'mode_remove']
+
+CHAR_SPACE: Final = ' '
+CHAR_DOUBLE_QUOTATION_MARK: Final = '⁇'
+CHAR_WHITE_QUESTION_MARK: Final = '❔'
+CHAR_RED_QUESTION_MARK: Final = '❓'
+
+
+class EmptyStringError(Exception):
+    pass
+
+
+def is_vaild_file_name(
+    name: str,
+) -> bool:
+    return (
+        all(char not in TRANSLATE_TABLE_REPLACEMENT for char in name)
+        and name.upper()[:3] not in NOT_ALLOWED_NAMES
+        and name.upper()[:4] not in NOT_ALLOWED_NAMES
+        and name.endswith('.')
+    )
+    # ...or return translate_to_safe_name(name) == name  # 훨씬 느림
+
+
+def safe_name_to_original_name(
+    name: str,
+    remove_replacement_char: str | None = None,
+    html_escape: bool = False,
+    consecutive_char: str | None = None,
+) -> str:
+    """translate_to_safe_name을 통해 안전한 이름으로 바꾸었던 것을 다시 일반적인 문자열로 변경합니다.
+
+    Args:
+        name (str): 일반적인 문자열로 되돌릴 안전한 이름입니다.
+        remove_replacement_char (str | None, optional): 만약 안전한 이름을 만들 때 사용했던 replacement_char가 있고 제거하고 싶다면 작성해 주세요.
+            해당 문자가 replacement_char로 간주되며 삭제됩니다. None(기본값)이라면 않았다면 아무것도 바꾸지 않습니다. Defaults to None.
+        html_escape (bool, optional): html unescape했던 것을 다시 되돌리고 싶다면 선택하세요. 만약 되돌리고 싶지 않다면 그대로 False로 두세요. Defaults to False.
+        consecutive_char (str | None, optional): consecutive_char를 알고 있고 다시 스페이스로 되돌리고 싶다면 작성해 주세요.
+            이때 기존에 이미 있었던 consecutive_char와 동일한 문자도 같이 변경될 수 있습니다.
+            예를 들어 기존 이름이 'Hello World! - by myself.txt'가 있고 consecutive_char가 '-'면 'Hello-World!---by-myself.txt'
+            가 되었을 것이고 consecutive_char 값을 '-'으로 설정하면 'Hello World!   by myself.txt'가 됩니다.
+            원본과는 달라졌을 수 있다는 의미입니다. Defaults to None.
+    """
+    upside_down_table: dict[int, int] = dict(map(reversed, TRANSLATE_TABLE_FULLWIDTH.items()))  # type: ignore
+    processed = name.translate(upside_down_table)
+
+    if remove_replacement_char is not None:
+        processed = processed.replace(remove_replacement_char, '')
+
+    if html_escape:
+        processed = html.escape(processed)
+
+    if consecutive_char is not None:
+        processed = processed.replace(consecutive_char, ' ')
+
+    return processed
+
+
+def translate_to_safe_path_name(
+    path: str | Path,
+    mode: TextModes = MODE_FULLWIDTH,
     *,
-    normalize=False,
-    html_unescape=True,
-    following_dot: Literal['one_dot_leader', 'fullwidth', 'add_zerowidth', 'add_underscore', 'remove', 'ignore'] = 'add_underscore',
-    handling_empty_string: str | None = None
-    ) -> str | Path:
-    """Translate string to filename-safe string.
-    이 함수는 
-    
-    
-    Caution: Don't put here diretory path beacause it will translate slash and backslash to acceptable(and cannot be used for going directory) name.
+    length_check: bool = True,
+    html_unescape: bool = True,
+    replacement_char: str = CHAR_SPACE,
+    correct_following_dot: CorrectFollowingDot = DOT_REPLACE,
+    consecutive_char: str | None = None,
+) -> str | Path:
+    r"""
+    경로를 직접 변경하기보다는 경로에 들어가는 각각의 파일이나 디렉토리 이름들을 모두 안전한 이름으로 바꾸고
+    사용할 것을 권장합니다.
+    예를 들어 사용자 입력이 "1/0은 무슨 값일까?"라면  "1" 디렉토리 아래에 "0은 무슨 값일까?"라는 파일이 있는 식으로
+    잘못 계산될 수 있습니다. translate_to_safe_name을 이용하면 이러한 문제가 발생하지 않습니다.
+    pathlib.Path는 이름이 실제로 작성 가능한지 여부를 따지지 않습니다. 따라서 실제로 사용 가능한 URL인지는 확신할 수 없습니다.
+    따라서 translate_to_safe_path_name를 통해 해당 디렉토리를 안전하게 만들고 싶을 수 있습니다.
+
+    Params:
+        path: 사용할 경로입니다.
+        length_check: Windows에는 경로를 포함한 파일의 이름이 255자가 넘어가면 해당 이름으로 파일을 만들 수 없습니다.
+            단순히 PurePath의 용도로 사용할 것이거나, 현재 경로가 바뀔 수 있거나 속도 향상을 원한다면 False로 설정해 끌 수 있습니다.
+        correct_following_dot: 이 값을 DOT_NO_CORRECTION으로 하려 한다면 디렉토리의 이름들까지 모두 변경되지 않기 때문에
+        특별한 주의가 필요합니다. 예를 들어 'hello./world.'이라는 경로가 있을 때 DOT_NO_CORRECTION을 사용하면 'hello.'만
+        변경되지 않는 것이 아닌 'hello.'의 이름도 변경되지 않습니다.
+        나머지 파라미터들과 correct_following_dot의 기본 설명은 translate_to_safe_name을 확인해 주세요.
+    """
+    is_path = not isinstance(path, str)
+
+    translated = [translate_to_safe_name(
+        path_part, mode=mode, html_unescape=html_unescape, correct_following_dot=correct_following_dot,
+        replacement_char=replacement_char, consecutive_char=consecutive_char
+    ) for path_part in Path(path).parts]
+
+    translated_path = Path('/'.join(translated))
+
+    if length_check and len(str(translated_path.absolute())) >= 246:
+        logging.warning('Your path is too long, so it might cannot be not saved or modified '
+                        'in case of you are using default settings in Windows.')
+
+    return translated_path if is_path else str(translated_path)
+
+
+def translate_to_safe_name(
+    name: str,
+    mode: TextModes = MODE_FULLWIDTH,
+    *,
+    html_unescape: bool = True,
+    replacement_char: str = CHAR_SPACE,
+    correct_following_dot: CorrectFollowingDot = DOT_REPLACE,
+    consecutive_char: str | None = None,
+) -> str:
+    """파일명 혹은 디렉토리명에 사용할 수 없는 글자를 사용할 수 있는 글자로 변경합니다.
+    이 함수는 이름을 normalize하는 것에 주안점을 두고 있지 않습니다. 대신 윈도우에서 이름 충돌이 일어나지 않도록 만듭니다.
+    주의: 이 함수는 디렉토리를 처리하도록 제작되지 않았습니다. 만약 디렉토리를 처리하야 한다면 
+
+    이 함수에서 처리되는
+    이 함수는 거의 모든 경우에서 함수의 부분에 대해서도 성립합니다. 하지만 마침표(.)의 경우는 그렇지 않습니다.
 
     params:
-        name: 파일명으로 사용할 이름입니다. 만약 'helloworld.txt'가 있다면 'helloworld.txt' 전체를 그대로 \
-            입력하시면 됩니다.
-        
-        name_property: 만약에 이 함수로 파일이 아닌 디렉토리 경로를 인코딩한다면, \이나 /은 더 이상 변경되어선 \
-            안 됩니다. 따라서 파일일 경우와 디렉토리 경로일 경우를 달리합니다.
-            file(기본값): 파일을 인코딩합니다. \와 / 모두 파일 이름에 사용 가능한 문자열로 변경됩니다.
-            path: 디렉토리를 인코딩합니다. \(backslash, Windows의 디렉토리 구분 문자)와 /(slash, Posix의 디렉토리 구분 문자)는 변경 대상에서 제외됩니다.
-            posix_path: 디렉토리를 인코딩합니다. /(slash, Posix의 디렉토리 구분 문자)는 변경 대상에서 제외됩니다.
-            windows_path: 디렉토리를 인코딩합니다. \(backslash, Windows의 디렉토리 구분 문자)는 변경 대상에서 제외됩니다.
-            파일 이름 자체에 \이나 /가 포함되어 있을 수 있기 때문에 path, posix_path, windows_path를
-            사용하기보다는 각각의 파일 이름을 인코딩하고 각자 경로로 합치는 것을 더욱 추천합니다.
-    
-        following_dot: 윈도우에서는 '.'가 파일이나 폴더명의 맨 마지막에 오지 못하도록 합니다. \
-        따라서 다음의 해결책들이 사용될 수 있습니다.
-            one_dot_leader: 가장 보기 좋은 해결책
-                ONE DOT LEADER('․')는 일반적인 점과 가장 유사한 모양을 가진 유니코드 문자열입니다.
-                대부분의 파일 관리자나 애플리케이션에서 잘 보이나 notepad++(정확히는 notepad++의 글꼴)등
-                이를 지원하지 않는 경우도 존재합니다.
-            fullwidth: 가장 호환성 좋은 해결책
-                FULLWIDTH FULL STOP('．')은 모양은 ONE DOT LEADER보다 유사성은 덜하지만,
-                호환성은 ONE DOT LEADER보다 좋습니다. 또한 mode를 fullwidth로 선택했다면 오히려 일관성 있는
-                선택이 될 수 있습니다. one_dot_leader가 싫다면 fullwidth를 고려하는 것이 가장 타당할 것입니다.
-            add_zerowidth: 아무런 차이가 없어 보여 좋지만 혼란스러울 수 있는 해결책
-                어쩌면 가장 좋은 해결책일지도 모릅니다. ZERO WIDTH SPACE(​)는 있는 문자지만 보이지 않습니다.
-                이를 문장의 맨 끝에 추가하면 '맨 뒤의 문자'가 .이 아니게 되면서 .을 제거하거나 바꿀 필요가 없어집니다.
-                다만, 보이지 않는 문자를 사용하는 해결방안이라 약간의 혼란을 야기할 수 있습니다.
-            add_underscore(기본값): ASCII 내에서 해결하고 싶은 사람에게 가장 좋은 픽
-                파이썬에서는 기존에 이미 정의되어있던 이름과 겹치는 이름의 변수를 만들고 싶은 경우 이름 뒤에
-                언더스코어(_)를 붙여 충돌을 피하고는 합니다. 이 상황은 이 파이썬의 규범과 잘 어울리기 때문에
-                충분히 pythonic한 해결책으로 볼 수 있습니다. 별로 예쁜 (만약 파일 이름이 '이게 맞나...'였다면
-                변환된 이름은 '이게 맞나..._'가 됩니다. 썩 예쁘진 않죠.) 선택지는 아닙니다만 무난한 선택지입니다.
-            remove: 가장 파괴적인 선택
-                말 그대로 뒤에 점을 모두 없애버립니다. 다만 잘못하면 모든 문자열이 사라져 비게 될 수도 있으니 주의하세요.
-            ignore: 
-                만약 파일의 끝에 무언가 더 추가할 예정이라면 사실 .의 존재 여부가 중요하지 않을 수도 있습니다.
-                예를 들어 '아무개 씨의 일기.'라는 제목을 처리한 뒤에 txt를 붙여 '아무개 씨의 일기..txt'로
-                만들 생각이라면 .의 처리가 그다지 필요하지 않을 것입니다. 그럴 때 ignore를 사용할 수 있습니다.
+        name: 파일 혹은 디렉토리의 이름입니다.
+            만약 'helloworld.txt'가 있다면 'helloworld.txt' 전체를 그대로 입력하시면 됩니다.
+            'is_vaild_file_name' 사용을 권장합니다.
 
+        mode: 파일 이름에서 사용될 수 없는 이름을
+
+        html_unescape:
+
+        correct_following_dot:
+            윈도우에서는 파일 이름 맨 끝에 
+
+        replacement_char:
+            만약 mode가 MODE_USE_REPLACEMENT_CHAR이면 사용할 수 없는 모든 글자가 replacement_char로 변환됩니다.
+            mode가 fullwidth일 때에는 일부 fullwidth character로 표현할 수 없는 문자(제어 문자 등.)가 replacement_char로 변환됩니다.
+            현재로서는 꼭 한 글자일 필요는 없고, 여러 글자도 사용 가능합니다.
+
+        consecutive_char:
+            만약 None(기본값)일 경우 아무런 영향도 주지 않지만, None이 아닐 경우 스페이스를 해당 문자로 변경합니다.
+            replacement_char가 스페이스(기본값)일 때 영향을 받을 수 있으니 주의하세요.
     """
-    assert name_property in ['file', 'path', 'posix_path', 'windows_path'], 'Unknown commend in name_property'
-    assert mode in ['fullwidth', 'space', 'remove'], 'Unknown commend in mode'
-    assert following_dot in ['one_dot_leader', 'fullwidth',
-                             'add_zerowidth', 'add_underscore',
-                             'remove', 'ignore'], 'Unknown commend in mode'
-    
-    
-    is_path = False
-    if isinstance(name, Path):
-        name = str(name)
-        is_path = True
-        if name_property == 'file':
-            logging.warning('using Path object but name_property is file.')
+    processed = html.unescape(name) if html_unescape else name
 
-
-    if html_unescape:
-        processed: str = html.unescape(name)  # change things like "&amp;" to "'".
-
-    table = {}
-    if mode == 'fullwidth':
-        table = str.maketrans(':*?"<>|\t\n', '：＊？＂＜＞∣   ')
-    if mode == 'space':
-        table = str.maketrans(':*?"<>|\t\n', '          ')
-    table.update(
-        {i : 32 for i in range(32)} # 0부터 31까지 모두 space로 바꿈
-    )
-
-    # directory인 경우, \나 /를 fullwidth로 변경하지 않음.
-    if name_property not in ("path", "windows_path"):
-        table.update({ord('\\'): ord('⧵')})
-    if name_property not in ("path", "posix_path"):
-        table.update({ord('/'): ord('／')})
+    processed = processed.translate(TRANSLATE_TABLE_FULLWIDTH if mode == MODE_FULLWIDTH else TRANSLATE_TABLE_REPLACEMENT)
 
     # 윈도우에서는 앞뒤에 space가 있을 수 없기에 strip이 필요하다.
-    if mode == 'remove':
-        unsupported = ''.join(chr(i) for i in range(32))
-        processed = re.sub('[:*?"<>|\t\n{}]'.format(unsupported), '', processed).strip()
-    else:
-        processed = processed.translate(table).strip()
+    processed = processed.replace('\x00', replacement_char).strip()
 
+    if correct_following_dot and processed.endswith('.'):
+        if correct_following_dot == DOT_REMOVE:
+            processed = processed.rstrip('.')
+        else:
+            processed = processed.removesuffix('.') + FOLLOWING_DOT_REPLACEMENT[correct_following_dot]
 
+    if not processed:
+        raise EmptyStringError(f'After processing, the string is empty. (input name: {name})')
 
-    if following_dot == 'remove':
-        processed = processed.rstrip('.')
-    else:
-        cases = {'one_dot_leader': '․', 'fullwidth': '．', 'add_zerowidth': '.\u200b', 'ignore': '.', 'add_underscore': '._'}
-        processed = re.sub(r'\.$', cases[following_dot], processed)
+    if processed.upper()[:3] in NOT_ALLOWED_NAMES or processed.upper()[:4] in NOT_ALLOWED_NAMES:
+        processed += '_'
 
-    # 빈 문자열일 경우 None으로 치환
-    if handling_empty_string and processed == '':
-        processed = handling_empty_string
+    if consecutive_char is not None:
+        processed = processed.replace(' ', consecutive_char)
 
-    return Path(processed) if is_path else processed
-
-
-if __name__ == "__main__":
-    print(safe_name('hello/world!?...', following_dot='one_dot_leader'))
-    print(safe_name('hello/world!?...', following_dot='fullwidth'))
-    print(safe_name('hello/world!?...', following_dot='add_zerowidth'))
-    print(safe_name('hello/world!?...', following_dot='ignore'))
-    print(safe_name('hello/world!?...', following_dot='add_underscore'))
-    print(safe_name('hello/world!?', following_dot='add_underscore'))
-    print(safe_name('.', following_dot='remove'))
-    print(repr(safe_name(Path("hello!?"))))
+    return processed
