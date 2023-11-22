6fa19cd89fb623e65769a420d34342e571dff6b7
ilotoki0804 <ilotoki0804@gmail.com>
2023-11-23 00:19:09 +0900

    절대 경로 import 제거

--- pyfilename/__init__.py
+++ pyfilename/__init__.py
@@ -1,14 +1,7 @@
-if __name__ == "__main__":
-    from main import (
-        TRANSLATE_TABLE_FULLWIDTH, TRANSLATE_TABLE_REPLACEMENT, NOT_ALLOWED_NAMES,
-        DotHandlingPolicy, TextMode, ReplacementCharacter,
-        is_name_safe, get_original_name, sanitize_path, sanitize, sanitize_return_value
-    )
-else:
-    from .main import (
-        TRANSLATE_TABLE_FULLWIDTH, TRANSLATE_TABLE_REPLACEMENT, NOT_ALLOWED_NAMES,
-        DotHandlingPolicy, TextMode, ReplacementCharacter,
-        is_name_safe, get_original_name, sanitize_path, sanitize, sanitize_return_value
-    )
+from .main import (
+    TRANSLATE_TABLE_FULLWIDTH, TRANSLATE_TABLE_REPLACEMENT, NOT_ALLOWED_NAMES,
+    DotHandlingPolicy, TextMode, ReplacementCharacter,
+    is_name_safe, get_original_name, sanitize_path, sanitize, sanitize_return_value
+)
 
 __version__ = "0.4.0"
